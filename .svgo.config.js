28795f0aed4a60c6cc0b52df96cd3cff791e6f54
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-02-13 18:53:28 +0000

    Added data-name and data-variant attributes

--- .svgo.config.js
+++ .svgo.config.js
@@ -9,9 +9,13 @@ module.exports = {
     {
       name: 'addFilenameAttribute',
       fn(root, _params, info) {
-        const {basename} = info.path.match(/.*\/(?<basename>.*)\.svg$/).groups;
+        const path = require('path');
+        const basename = path.basename(info.path, '.svg');
+        const folder = path.basename(path.dirname(info.path));
+
         if (root.children[0].name === 'svg') {
-          root.children[0].attributes['data-id'] = basename;
+          root.children[0].attributes['data-name'] = basename;
+          root.children[0].attributes['data-variant'] = folder;
         }
       }
     }
